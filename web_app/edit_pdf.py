9d5365e0729d705626f0cf0738aedf8947bfd1e9
Badr El Mazaz <59953925+Fermin-Romero-De-Torres@users.noreply.github.com>
2022-01-29 14:29:12 +0100

    heroku

--- web_app/edit_pdf.py
+++ web_app/edit_pdf.py
@@ -13,7 +13,8 @@ def create_pdf(data: list[dict]):
         path_to_file = os.path.join(os.getcwd(), "web_app", "tmp", file_name)
         for d in data:
             data_dict.update(d["answer"])
-        fillpdfs.write_fillable_pdf(r".\web_app\static\modulo.pdf", path_to_file, data_dict)
+        modulo_path=os.path.join(os.getcwd(), "web_app", "static", "modulo.pdf")
+        fillpdfs.write_fillable_pdf(modulo_path, path_to_file, data_dict)
         file_name2 = str(uuid4()) + ".pdf"
         path_to_file2 = os.path.join(os.getcwd(), "static_download", file_name2)
         print(file_name2)
