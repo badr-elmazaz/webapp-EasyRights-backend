c0f7c236ed50f0ba3d43ab78e3dac79b8953a4e8
Badr El Mazaz <badr.elmazaz@eng.it>
2022-04-12 17:56:40 +0200

    updated the translator

--- main.py
+++ main.py
@@ -128,4 +128,4 @@ async def block(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8638)
+    uvicorn.run(app, host="0.0.0.0", port=8638)
--- translate_json.py
+++ translate_json.py
@@ -1,5 +1,7 @@
 import deepl
-
+from googletrans import Translator
+translator = Translator()
+import traceback
 import json
 
 
@@ -7,14 +9,21 @@ import json
 # print(translation)
 
 def trans(a: str):
-    return deepl.translate(source_language="EN", target_language="AR", text=a)
+    # return deepl.translate(source_language="EN", target_language="AR", text=a)
+    print(f"trying to translate this sentence: {a}")
+    try:
+        txt= translator.translate(a, src='en',dest="uk")
+        return txt.text
+    except:
+        return "ERROR TRANSLATING"
+        traceback.print_exc()
 
 
 with open("languages.json") as f:
     json_en=json.load(f)
 
 # translate to Italian
-for k in json_en["questions"]:
+for k in json_en:
     if "label" in k.keys():
         k["label"] = trans(k["label"])
     if "title" in k.keys():
@@ -26,5 +35,5 @@ for k in json_en["questions"]:
             if "helperText" in j.keys():
                 j["helperText"] = trans(j["helperText"])
 
-with open("espanol.json", "w") as f:
+with open("ukrainian.json", "w") as f:
     json.dump(json_en, f)
